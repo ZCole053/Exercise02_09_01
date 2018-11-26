f25561a691e773da73a0e80f65936313142fd454
Unknown <zcole053@west-mec.org>
2018-11-26 14:25:57 -0800

    Database query
    I added a new sftp to upload my files to the sever. We are grabbing the data in the table and we are making a selection list out of it.After which we started making a table.

--- AvailableOpportunities.php
+++ AvailableOpportunities.php
@@ -76,11 +76,70 @@
 
      echo "\$internName: $internName";//debug
 
+     //Gets the count of the id number that is the same and data approved has 
+     //something in it.
+     $tableName = "assigned_opportunities";
+     if($errors == 0){
+        $SQLstring = "SELECT COUNT(opportunityID)". 
+            " FROM $tableName". 
+            " WHERE internID='$internID'".
+            " AND dateApproved IS NOT NULL";
+        $queryResult = mysqli_query($DBConnect,$SQLstring);
+        if(mysqli_num_rows($queryResult) > 0){
+            //puts everything in query into an indexed array
+            $row = mysqli_fetch_row($queryResult);
+            $approvedOpportunities = $row[0];
+            //frees up query space
+            mysqli_free_result($queryResult);
+        }
+        //array to hold results
+        $selectedOpportunities = array();
+        $SQLstring = "SELECT opportunityID FROM $tableName".
+        " WHERE dateApproved IS NOT NULL";
+        //could hav nothing or something
+        $queryResult = mysqli_query($DBConnect,$SQLstring);
+        if(mysqli_num_rows($queryResult) > 0){
+            while(($row = mysqli_fetch_row($queryResult)) != false){
+                $selectedOpportunities[] = $row[0];
+            }
+            mysqli_free_result($queryResult);
+        }
+        //if it is assigned to any intern
+        $assignedOpportunities = array();
+        $SQLstring = "SELECT opportunityID FROM $tableName".
+        " WHERE dateApproved IS NOT NULL";
+        $queryResult = mysqli_query($DBConnect,$SQLstring);
+        if(mysqli_num_rows($queryResult) > 0){
+            while(($row = mysqli_fetch_row($queryResult)) != false){
+                $assignedOpportunities[] = $row[0];
+            }
+            mysqli_free_result($queryResult);
+        }
+        //change table name
+        $tableName = "opportunities";
+        $opportunities = array();
+        $SQLstring = "SELECT opportunityID, company, city,".
+        " startDate, endDate, position, description".
+        " From $tableName";
+        $queryResult = mysqli_query($DBConnect,$SQLstring);
+        if(mysqli_num_rows($queryResult) > 0){
+            while(($row = mysqli_fetch_assoc($queryResult)) != false){
+                $opportunities[] = $row;
+            }
+            mysqli_free_result($queryResult);
+        }
+    }
+
      //closes database
      if($DBConnect){
         echo "<p>Closing database \"$DBName\" connection.</p>\n";//debug
         mysqli_close($DBConnect);
      }
+
+     echo "<table border='1' width='100%>\n'";
+     echo "</table>\n";
+     echo "<p><a href='InternLogin.php'>Log Out</a></p>\n";
+
     ?>
 </body>
 </html>
